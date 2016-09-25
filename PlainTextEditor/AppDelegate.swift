618de409dde00929c37b8ae0d1d8c343cf42aa4d
1024jp <1024jp@wolfrosch.com>
2016-09-26 07:29:06 +0900

    Initial Commit

---
+++ PlainTextEditor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PlainTextEditor
+//
+//  Created by 1024jp on 2016/09/26.
+//  © 2016 1024jp
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
